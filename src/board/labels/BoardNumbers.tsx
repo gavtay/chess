31cb2e1f2b67daf00892df02ca7115186d5d64a1
Kataneki <gavintaylor.ai@gmail.com>
2024-04-06 18:18:24 -0500

    Flipped the number order of the board row labels.

--- src/board/labels/BoardNumbers.tsx
+++ src/board/labels/BoardNumbers.tsx
@@ -4,14 +4,14 @@ function BoardNumbers() {
     return (
         <>
             <div className='boardnumbers-container'>
-                <div className='number-container'><p>1</p></div>
-                <div className='number-container'><p>2</p></div>
-                <div className='number-container'><p>3</p></div>
-                <div className='number-container'><p>4</p></div>
-                <div className='number-container'><p>5</p></div>
-                <div className='number-container'><p>6</p></div>
-                <div className='number-container'><p>7</p></div>
                 <div className='number-container'><p>8</p></div>
+                <div className='number-container'><p>7</p></div>
+                <div className='number-container'><p>6</p></div>
+                <div className='number-container'><p>5</p></div>
+                <div className='number-container'><p>4</p></div>
+                <div className='number-container'><p>3</p></div>
+                <div className='number-container'><p>2</p></div>
+                <div className='number-container'><p>1</p></div>
             </div>
         </>
     )
